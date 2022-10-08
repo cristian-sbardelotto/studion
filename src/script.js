aab6c4b0761be85c291a0439c2736bff11701323
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-08 10:56:51 -0300

    feat(responsiveness): finished the responsiveness concepts

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
 $(document).ready(() => {
+  //  MENU
+$('.navbar__menu-btn').on('click', function () {
+  $('.navbar__links').toggleClass('active');
+  $(this).find('i').toggleClass("fa-bars");
+  $(this).find('i').toggleClass("fa-times");
+})
+
+  // CARROSSEL
   const slickOptions = {
     dots: false,
     autoplay: true,
@@ -7,6 +15,8 @@ $(document).ready(() => {
   };
   $('.slider').slick(slickOptions);
 
+
+  // FOOTER
   $('.footer__form-button').on('click', () => {
     const email = $('#email').val();
     email.send({
